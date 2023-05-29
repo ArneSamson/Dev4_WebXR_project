8876990469ad0b6ed93d749cb0816e9042fd2fa8
Yorick Devleeschouwer <64488176+yoyoplayer12@users.noreply.github.com>
2023-05-29 13:01:02 +0200

    fix: balloon now moves 1 time

--- main.js
+++ main.js
@@ -17,67 +17,67 @@ init();
 animate();
 
 function init() {
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x808080);
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color(0x808080);
 
-  camera = new THREE.PerspectiveCamera(
-    80,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    10
-  );
-  camera.position.set(0, 1.2, 0.3);
+	camera = new THREE.PerspectiveCamera(
+		80,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		10
+	);
+	camera.position.set(0, 1.2, 0.3);
 
-  scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
+	scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
 
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(0, 6, 0);
-  light.castShadow = true;
-  light.shadow.camera.top = 2;
-  light.shadow.camera.bottom = -2;
-  light.shadow.camera.right = 2;
-  light.shadow.camera.left = -2;
-  light.shadow.mapSize.set(4096, 4096);
-  scene.add(light);
+	const light = new THREE.DirectionalLight(0xffffff);
+	light.position.set(0, 6, 0);
+	light.castShadow = true;
+	light.shadow.camera.top = 2;
+	light.shadow.camera.bottom = -2;
+	light.shadow.camera.right = 2;
+	light.shadow.camera.left = -2;
+	light.shadow.mapSize.set(4096, 4096);
+	scene.add(light);
 
-  const floorGeometry = new THREE.PlaneGeometry(6, 6);
-  const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x595959 });
-  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.rotation.x = -Math.PI / 2;
-  floor.receiveShadow = true;
-  scene.add(floor);
+	const floorGeometry = new THREE.PlaneGeometry(6, 6);
+	const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x595959 });
+	const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	floor.rotation.x = -Math.PI / 2;
+	floor.receiveShadow = true;
+	scene.add(floor);
 
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  renderer.shadowMap.enabled = true;
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.shadowMap.enabled = true;
 
-  document.body.appendChild(renderer.domElement);
-  document.body.appendChild(VRButton.createButton(renderer));
+	document.body.appendChild(renderer.domElement);
+	document.body.appendChild(VRButton.createButton(renderer));
 
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.target.set(0, 1.6, 0);
-  controls.update();
+	controls = new OrbitControls(camera, renderer.domElement);
+	controls.target.set(0, 1.6, 0);
+	controls.update();
 
-  navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
-    if (supported) {
-      renderer.xr.enabled = true;
-      renderer.xr.setReferenceSpaceType('local');
-      renderer.xr.setSession('immersive-vr').then((session) => {
-        vrDisplay = session.display;
-        vrFrameData = new VRFrameData();
-      });
-    }
-  });
+	navigator.xr.isSessionSupported('immersive-vr').then((supported) => {
+		if (supported) {
+			renderer.xr.enabled = true;
+			renderer.xr.setReferenceSpaceType('local');
+			renderer.xr.setSession('immersive-vr').then((session) => {
+				vrDisplay = session.display;
+				vrFrameData = new VRFrameData();
+			});
+		}
+	});
 
-  loadBalloonModel();
-  loadDartModel("blue");
-  loadDartModel("red");
+	loadBalloonModel();
+	loadDartModel("blue");
+	loadDartModel("red");
 
-  window.addEventListener('resize', onWindowResize, false);
+	window.addEventListener('resize', onWindowResize, false);
 
-  renderer.domElement.addEventListener('click', shootDart);
+	renderer.domElement.addEventListener('click', shootDart);
 }
 
 // Shooting stand dimensions
@@ -100,7 +100,7 @@ woodTexture.wrapS = THREE.RepeatWrapping;
 
 //______________________________________________________________________
 // Create the missing half face
-const missingFaceGeometry = new THREE.BoxGeometry(standWidth * 1.01, standHeight / 2, standDepth/30);
+const missingFaceGeometry = new THREE.BoxGeometry(standWidth * 1.01, standHeight / 2, standDepth / 30);
 const missingFaceMaterial = new THREE.MeshBasicMaterial({ map: whiteRedTexture });
 const missingFace = new THREE.Mesh(missingFaceGeometry, missingFaceMaterial);
 missingFace.position.set(0, 0.25, -1.03); // Adjust the position as needed
@@ -134,144 +134,146 @@ scene.add(backWall);
 function loadBalloonModel() {
 	const loader = new GLTFLoader();
 	loader.load('assets/balloon/scene.gltf', function (gltf) {
-	  const newBalloon = gltf.scene;
-
-	  	newBalloon.scale.set(0.005, 0.005, 0.005);
-	  	newBalloon.position.set(0, 0, -2); // Adjust the position as needed
-		scene.add(newBalloon);
-		    // Update the reference to the new balloon
-			balloon = newBalloon;
-
-			// Restart the animation
-			animate();
+		const newBalloon = gltf.scene;
+		newBalloon.scale.set(0.005, 0.005, 0.005);
+		newBalloon.position.set(2, 0, -2); // Adjust the position as needed 
+		
+		// Update the reference to the new balloon
+		balloon = newBalloon;
+		scene.add(balloon);
+		// Restart the animation
+		animate();
 		// Iterate over the materials defined in the GLTF model
-		gltf.scene.traverse(function (child) {
-		if (child.isMesh) {
-		  const materials = child.material;
-  
-		  if (Array.isArray(materials)) {
-			// Iterate over multiple materials (if applicable)
-			materials.forEach(function (material) {
-			  // Apply the material to the corresponding object
-			  material.color = new THREE.Color(material.color);
-			  child.material = material;
-			});
-		  } else {
-			// Single material case
-			materials.color = new THREE.Color(materials.color);
-			child.material = materials;
-		  }
-		}
-	  });
-  });
+		// gltf.scene.traverse(function (child) {
+		// 	if (child.isMesh) {
+		// 		const materials = child.material;
+
+		// 		if (Array.isArray(materials)) {
+		// 			// Iterate over multiple materials (if applicable)
+		// 			materials.forEach(function (material) {
+		// 				// Apply the material to the corresponding object
+		// 				material.color = new THREE.Color(material.color);
+		// 				child.material = material;
+		// 			});
+		// 		} else {
+		// 			// Single material case
+		// 			materials.color = new THREE.Color(materials.color);
+		// 			child.material = materials;
+		// 		}
+		// 	}
+		// });
+	});
 }
+
+let isBalloonRemoved = false;
 
 function animateBalloon() {
-	if (!balloon) {
-		return; // Stop the animation if balloon is not present
-	  }
-	if (balloon) {
-		const loaded = 0;
-		// Calculate the vertical position offset using a sine wave
-		const time = performance.now() * 0.001; // Convert time to seconds
-		const yOffset = Math.sin(time * 2) * 0.25; // Adjust the amplitude and speed as needed
-		let xOffset = (time * 2) * 0.25; // Adjust the amplitude and speed as needed
-		// Update the balloon's position
-		balloon.position.y = 0 + yOffset;
-		balloon.position.x = 2 - xOffset;
-		if (balloon.position.x < leftWall.position.x + leftWall.geometry.parameters.width / 2) {
-			// Remove the current balloon from the scene
-			scene.remove(balloon);
-			balloon = null; // Reset the balloon reference
-			console.log("Game over!");
-			// Create a new balloon and add it to the scene
-			if (balloon = null) {
-				loadBalloonModel();
-				return; // Stop the animation if balloon is not present
-			  }
-		  }
-	}
+  if (isBalloonRemoved) {
+    return; // Stop the animation if balloon is removed
+  }
+
+  if (balloon) {
+	console.log("balloon");
+    // Calculate the vertical position offset using a sine wave
+    const time = performance.now() * 0.001; // Convert time to seconds
+    const yOffset = Math.sin(time * 2) * 0.25; // Adjust the amplitude and speed as needed
+    let xOffset = (time * 2) * 0.25; // Adjust the amplitude and speed as needed
+    // Update the balloon's position
+    balloon.position.y = 0 + yOffset;
+    balloon.position.x = 2 - xOffset;
+    if (balloon.position.x < leftWall.position.x + leftWall.geometry.parameters.width / 2) {
+      // Remove the current balloon from the scene
+      console.log("passed wall");
+      scene.remove(balloon);
+      balloon = null; // Reset the balloon reference
+      isBalloonRemoved = true; // Set the flag to true
+      // Create a new balloon and add it to the scene
+      loadBalloonModel();
+      return; // Stop the animation if balloon is not present
+    }
+  }
 }
+
 
 //dart_________________________________________________________________
 function loadDartModel(color) {
-  return new Promise((resolve, reject) => {
-    const loader = new GLTFLoader();
+	return new Promise((resolve, reject) => {
+		const loader = new GLTFLoader();
 
-    loader.load(`assets/darts/${color}/scene.gltf`, function (gltf) {
-      const dart = gltf.scene;
+		loader.load(`assets/darts/${color}/scene.gltf`, function (gltf) {
+			const dart = gltf.scene;
 
-      dart.scale.set(0.01, 0.01, 0.01);
+			dart.scale.set(0.01, 0.01, 0.01);
 
-      dart.traverse(function (child) {
-        if (child.isMesh) {
-          child.castShadow = true;
-        }
-      });
+			dart.traverse(function (child) {
+				if (child.isMesh) {
+					child.castShadow = true;
+				}
+			});
 
-      resolve(dart);
-    }, undefined, reject);
-  });
+			resolve(dart);
+		}, undefined, reject);
+	});
 }
 
 function shootDart(event) {
-  const controller = renderer.xr.getController(0);
+	const controller = renderer.xr.getController(0);
 
-  loadDartModel("red")
-    .then((loadedDart) => {
-      const dart = loadedDart;
-      dart.position.copy(controller.position);
-      dart.quaternion.copy(controller.quaternion);
-      dart.velocity = new THREE.Vector3();
-      dart.velocity.x = -dartSpeed;
-      dart.velocity.applyQuaternion(controller.quaternion);
+	loadDartModel("red")
+		.then((loadedDart) => {
+			const dart = loadedDart;
+			dart.position.copy(controller.position);
+			dart.quaternion.copy(controller.quaternion);
+			dart.velocity = new THREE.Vector3();
+			dart.velocity.x = -dartSpeed;
+			dart.velocity.applyQuaternion(controller.quaternion);
 
-      scene.add(dart);
-      activeDarts.push({ dart, controller });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+			scene.add(dart);
+			activeDarts.push({ dart, controller });
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 }
 
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-  	renderer.setAnimationLoop(render);
-	  renderer.setAnimationLoop(() => {
+	renderer.setAnimationLoop(render);
+	renderer.setAnimationLoop(() => {
 		render();
 		animateBalloon();
-	  });
+	});
 }
 
 function render() {
-  const delta = vrFrameData ? vrFrameData.deltaTime : 0.01;
+	const delta = vrFrameData ? vrFrameData.deltaTime : 0.01;
 
-  activeDarts.forEach((activeDart) => {
-    const { dart, controller } = activeDart;
+	activeDarts.forEach((activeDart) => {
+		const { dart, controller } = activeDart;
 
-    dart.position.addScaledVector(dart.velocity, delta);
-	if(balloon){
-		if (dart.position.distanceTo(balloon.position) < 0.1) {
+		dart.position.addScaledVector(dart.velocity, delta);
+		if (balloon) {
+			if (dart.position.distanceTo(balloon.position) < 0.1) {
+				scene.remove(dart);
+				activeDarts.splice(activeDarts.indexOf(activeDart), 1);
+				console.log('Dart hit the balloon!');
+			}
+		}
+
+
+		if (dart.position.y < 0 || Math.abs(dart.position.x) > 3 || Math.abs(dart.position.z) > 3) {
 			scene.remove(dart);
 			activeDarts.splice(activeDarts.indexOf(activeDart), 1);
-			console.log('Dart hit the balloon!');
-		  }
-	}
-    
+			console.log('Dart went out of bounds!');
+		}
+	});
 
-    if (dart.position.y < 0 || Math.abs(dart.position.x) > 3 || Math.abs(dart.position.z) > 3) {
-      scene.remove(dart);
-      activeDarts.splice(activeDarts.indexOf(activeDart), 1);
-      console.log('Dart went out of bounds!');
-    }
-  });
-
-  renderer.render(scene, camera);
-  animateBalloon();
+	renderer.render(scene, camera);
+	animateBalloon();
 }
